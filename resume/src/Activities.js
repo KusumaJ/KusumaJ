c86aa23619be88cb85d16564189c1101fb7cc4df
Kusuma <kusuma.j@columbia.edu>
2025-04-01 16:01:26 -0400

    Upload activity images

--- resume/src/Activities.js
+++ resume/src/Activities.js
@@ -9,7 +9,12 @@ function Activities() {
                     <div className="col-md-4 mb-4" key={index}>
                         <div className="card h-100">
                             {
-                                <img src={activity.img} className="card-img-top" alt={activity.name} style={{ height: '200px', objectFit: 'cover' }} />
+                                <img 
+                                src={require(`${activity.img}`)} 
+                                className="card-img-top" 
+                                alt={activity.name} 
+                                style={{ height: '200px', objectFit: 'cover' }} 
+                            />
                             }
                             <div className="card-body">
                                 <h5 className="card-title">{activity.name}</h5>
